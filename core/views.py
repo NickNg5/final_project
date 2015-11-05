31302486eab9f26ae052fe359b8a25ffd6d820ef
Nick <Nicholas.Ng@quinnipiac.edu>
2015-11-05 21:04:34 +0000

    Add business create view

--- core/views.py
+++ core/views.py
@@ -1,4 +1,17 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
 
 class Home(TemplateView):
-    template_name = "home.html"
+    template_name = 'home.html'
+
+class BusinessCreateView(CreateView):
+    model = Business
+    template_name = 'business/business_form.html'
+    fields = ['title', 'description']
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(BusinessCreateView, self).form_valid(form)
