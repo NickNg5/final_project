6dc3a29b96c8370ff954412eba854071a9d8903e
Nick <Nicholas.Ng@quinnipiac.edu>
2015-11-11 00:07:06 +0000

    Add required user authentication

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns = patterns('',
      url(r'^$', Home.as_view(), name='home'),
      url(r'^user/', include('registration.backends.simple.urls')),
      url(r'^user/', include('django.contrib.auth.urls')),
-     url(r'^business/create/$', BusinessCreateView.as_view(), name='business_create'),
-     url(r'^business/$', BusinessListView.as_view(), name='business_list'),
-     url(r'^business/(?P<pk>\d+)/$', BusinessDetailView.as_view(), name='business_detail'),
-     url(r'^business/update/(?P<pk>\d+)/$', BusinessUpdateView.as_view(), name='business_update'),
-     url(r'^business/delete/(?P<pk>\d+)/$', BusinessDeleteView.as_view(), name='business_delete'),
-     url(r'^business/(?P<pk>\d+)/comment/create/$', CommentCreateView.as_view(), name='comment_create'),
-     url(r'^business/(?P<business_pk>\d+)/comment/update/(?P<comment_pk>\d+)/$', CommentUpdateView.as_view(), name='comment_update'),
-     url(r'^business/(?P<business_pk>\d+)/comment/delete/(?P<comment_pk>\d+)/$', CommentDeleteView.as_view(), name='comment_delete'),
+     url(r'^business/create/$', login_required(BusinessCreateView.as_view()), name='business_create'),
+     url(r'^business/$', login_required(BusinessListView.as_view()), name='business_list'),
+     url(r'^business/(?P<pk>\d+)/$', login_required(BusinessDetailView.as_view()), name='business_detail'),
+     url(r'^business/update/(?P<pk>\d+)/$', login_required(BusinessUpdateView.as_view()), name='business_update'),
+     url(r'^business/delete/(?P<pk>\d+)/$', login_required(BusinessDeleteView.as_view()), name='business_delete'),
+     url(r'^business/(?P<pk>\d+)/comment/create/$', login_required(CommentCreateView.as_view()), name='comment_create'),
+     url(r'^business/(?P<business_pk>\d+)/comment/update/(?P<comment_pk>\d+)/$', login_required(CommentUpdateView.as_view()), name='comment_update'),
+     url(r'^business/(?P<business_pk>\d+)/comment/delete/(?P<comment_pk>\d+)/$', login_required(CommentDeleteView.as_view()), name='comment_delete'),
 )
